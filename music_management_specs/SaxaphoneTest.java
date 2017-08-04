e9b4f4776d94cda5cde7d97521f74f82b068667b
Travis Grimmond <travisslack1994@outlook.com>
2017-08-04 12:46:13 +0100

    three basic tests made and passed in saxaphone

--- music_management_specs/SaxaphoneTest.java
+++ music_management_specs/SaxaphoneTest.java
@@ -1,3 +1,28 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import music_management.*;
+
+public class SaxaphoneTest {
+
+  Saxaphone saxaphone;
+
+  @Before
+  public void before() {
+    saxaphone = new Saxaphone("Gold", "Brass", "Baritone");
+  }
+
+  @Test
+  public void hasColour() {
+    assertEquals ("Gold", saxaphone.getColourOfInstrument() );
+  }
+
+  @Test
+  public void typeofMaterial(){
+    assertEquals("Brass", saxaphone.getTypeOfMaterial() );
+  }
+
+  @Test
+  public void typeofInstrument() {
+    assertEquals("Baritone", saxaphone.getTypeOfInstrument() );
+  }
+}
